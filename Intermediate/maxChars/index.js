44924976afc96d52581d3af9a8d55aeade6824f2
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-04-29 16:44:18 +0530

    max chars

---
+++ Intermediate/maxChars/index.js
@@ -0,0 +1,43 @@
+// --- Directions
+// Given a string, return the character that 
+// is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+let maxChar = (str) => {
+	let chars = {}
+	let max = 0
+	let maxChar = ''
+
+	for (let char of str) {
+		if(!chars[char]) {
+			chars[char] = 1
+		} else {
+			chars[char]++
+		}
+	}
+
+	for (let char in chars) {
+		if(chars[char] > max){
+			max = chars[char]
+			maxChar = char
+		}
+	}
+
+	return maxChar
+}
+console.log(maxChar("aaccbbbbbbb"))
+
+/*
+Alternativelyy we could have used short syntax for the loop
+
+for (let chars if str) {
+	chars[char] = chars[char]++ || 1;
+}
+
+*/
+
+module.exports = maxChar;
+
